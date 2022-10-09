449c2c23c0f4823a6bd2262ea92079f80d506b5e
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-10-09 19:13:48 -0300

    added fix

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const textOfMeme = document.getElementById('meme-text');
 const container = document.getElementById('meme-image-container');
 const imagem = document.getElementById('meme-insert');
 const img = document.getElementById('img')
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
 
 const leitorDeArquivos = new FileReader();
 
@@ -13,7 +16,20 @@ input.addEventListener('input', function () {
 imagem.addEventListener('input', function() {
   leitorDeArquivos.readAsDataURL(this.files[0]);
   leitorDeArquivos.addEventListener('loadend', function(event){
-    container.style.backgroundImage = `url('${event.target.result}')`
-    container.style.backgroundSize = 'cover'
+    img.src = event.target.result
   })
+})
+
+function changerColorBackground(color) {
+  container.style.borderColor = color;
+}
+
+fire.addEventListener('click', () =>{
+  changerColorBackground('rgb(255, 0, 0)')
+})
+water.addEventListener('click', () => {
+  changerColorBackground('rgb(0, 0, 255)')
+})
+earth.addEventListener('click', () => {
+  changerColorBackground('rgb(0, 128, 0)')
 })
