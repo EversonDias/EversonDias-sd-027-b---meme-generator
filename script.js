76de8f46c6d46683f5508d3a847cdbe0b78888b4
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-10-09 11:51:10 -0300

    added container

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const input = document.getElementById('text-input');
 const textOfMeme = document.getElementById('meme-text');
-const preview = document.getElementById('mem-image-container');
+const container = document.getElementById('meme-image-container');
+const imagem = document.getElementById('meme-insert');
+const img = document.getElementById('img')
+
+const leitorDeArquivos = new FileReader();
 
 input.addEventListener('input', function () {
   textOfMeme.innerHTML = this.value;
 })
 
+imagem.addEventListener('input', function() {
+  leitorDeArquivos.readAsDataURL(this.files[0]);
+  leitorDeArquivos.addEventListener('loadend', function(event){
+    container.style.backgroundImage = `url('${event.target.result}')`
+    container.style.backgroundSize = 'cover'
+  })
+})
