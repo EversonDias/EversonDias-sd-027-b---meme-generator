05fde6eb231b6829e4bde304ba17da6ba05866d8
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-10-09 21:13:49 -0300

    added lint and script

--- script.js
+++ script.js
@@ -2,34 +2,57 @@ const input = document.getElementById('text-input');
 const textOfMeme = document.getElementById('meme-text');
 const container = document.getElementById('meme-image-container');
 const imagem = document.getElementById('meme-insert');
-const img = document.getElementById('img')
+const img = document.getElementById('meme-image');
 const fire = document.getElementById('fire');
 const water = document.getElementById('water');
 const earth = document.getElementById('earth');
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
 const leitorDeArquivos = new FileReader();
 
-input.addEventListener('input', function () {
-  textOfMeme.innerHTML = this.value;
-})
+input.addEventListener('input', () => {
+  textOfMeme.innerHTML = input.value;
+});
 
-imagem.addEventListener('input', function() {
-  leitorDeArquivos.readAsDataURL(this.files[0]);
-  leitorDeArquivos.addEventListener('loadend', function(event){
-    img.src = event.target.result
-  })
-})
+imagem.addEventListener('input', () => {
+  leitorDeArquivos.readAsDataURL(input.files[0]);
+  leitorDeArquivos.addEventListener('loadend', (event) => {
+    img.src = event.target.result;
+  });
+});
 
-function changerColorBackground(color) {
+function changerColorBackground(color, size, type) {
   container.style.borderColor = color;
+  container.style.borderWidth = size;
+  container.style.borderStyle = type;
 }
 
-fire.addEventListener('click', () =>{
-  changerColorBackground('rgb(255, 0, 0)')
-})
+fire.addEventListener('click', () => {
+  changerColorBackground('rgb(255, 0, 0)', '3px', 'dashed');
+});
 water.addEventListener('click', () => {
-  changerColorBackground('rgb(0, 0, 255)')
-})
+  changerColorBackground('rgb(0, 0, 255)', '5px', 'double');
+});
 earth.addEventListener('click', () => {
-  changerColorBackground('rgb(0, 128, 0)')
-})
+  changerColorBackground('rgb(0, 128, 0)', '6px', 'groove');
+});
+
+function changerBackground(props) {
+  img.src = props;
+}
+
+meme1.addEventListener('click', () => {
+  changerBackground('./imgs/meme1.png');
+});
+meme2.addEventListener('click', () => {
+  changerBackground('./imgs/meme2.png');
+});
+meme3.addEventListener('click', () => {
+  changerBackground('./imgs/meme3.png');
+});
+meme4.addEventListener('click', () => {
+  changerBackground('./imgs/meme4.png');
+});
